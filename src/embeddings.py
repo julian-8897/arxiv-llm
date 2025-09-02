d17e4db1a97c1fc80e55a8f2e8922097f66b4dfa
julian-8897 <julianchan8897@gmail.com>
2025-09-03 00:05:13 +0100

    Update default model name in TextEmbedder to allenai-specter

--- src/embeddings.py
+++ src/embeddings.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer
 
 
 class TextEmbedder:
-    def __init__(self, model_name: str = "sentence-transformers/all-MiniLM-L6-v2"):
+    def __init__(self, model_name: str = "sentence-transformers/allenai-specter"):
         self.model_name = model_name
         self.model = SentenceTransformer(self.model_name)
 
